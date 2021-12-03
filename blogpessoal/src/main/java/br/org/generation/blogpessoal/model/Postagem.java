4f783b30e176cc26f7840b465a5f6ef37af13232
Cristiane Damaceno <cristinecarmozini@gmail.com>
2021-12-02 21:37:02 -0300

    Atualizando exercicios

--- blogpessoal/src/main/java/br/org/generation/blogpessoal/model/Postagem.java
+++ blogpessoal/src/main/java/br/org/generation/blogpessoal/model/Postagem.java
@@ -7,36 +7,41 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
-@Entity // create table 
-@Table(name = "tb_postagens") // tb_postagens
+
+@Entity
+@Table(name = "tb_postagens") 
 public class Postagem {
+
 	
-	@Id // PRIMARY KEY (ID)
-	@GeneratedValue(strategy = GenerationType.IDENTITY) // autoincrement
-	private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	private long id; 
+
 	
-	@NotBlank(message = "O atributo titulo é Obrigatório!")
-	@Size(min = 5, max = 100, message =  "O atributo titulo deve ter no mínimo 5 e no máximo 100 caracteres")
-	private String titulo;
-	
+	@NotBlank(message = "O atributo título é Obrigatório e não pode utilizar espaços em branco!") 
+	@Size(min = 5, max = 100, message = "O atributo título deve conter no mínimo 05 e no máximo 100 caracteres")
+	private String titulo; 
+
 	@NotNull(message = "O atributo texto é Obrigatório!")
-	@Size(min = 10, max = 1000, message =  "O atributo texto deve ter no mínimo 10 e no máximo 1000 caracteres")
+	@Size(min = 10, max = 1000, message = "O atributo texto deve conter no mínimo 10 e no máximo 500 caracteres")
 	private String texto;
+
 	
 	@UpdateTimestamp
 	private LocalDate data;
 
-	public Long getId() {
+	
+	public long getId() {
 		return id;
 	}
 
-	public void setId(Long id) {
+	public void setId(long id) {
 		this.id = id;
 	}
 
@@ -63,6 +68,5 @@ public class Postagem {
 	public void setData(LocalDate data) {
 		this.data = data;
 	}
-	
-	
+
 }
